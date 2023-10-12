550d5ad64396c894074e4c5648d466dde549ed15
Eleni Marli <elenimarli@yahoo.gr>
2023-10-12 13:02:53 +0200

    Add help window for the GUI

--- main/app_gui.py
+++ main/app_gui.py
@@ -8,41 +8,64 @@ from main.main_commands import from_filenames_to_shopping_list
 
 import random
 
-from main.paths_config import recipes_full_path, result_full_path
+from main.paths_config import recipes_full_path, result_full_path, help_text_full_path
 
 all_filenames_as_list_of_str = os.listdir(recipes_full_path)
 
+# playing with the themes:
 themes = sg.list_of_look_and_feel_values()
 chosen_theme = random.choice(themes)
 print('The theme today is ' + chosen_theme)
 
 sg.theme(chosen_theme)  # (previously 'DarkAmber')
 # sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Choose recipes:', font = ('default', 11, 'bold'))],
-            [sg.Listbox( values= all_filenames_as_list_of_str, size=(30, 6), select_mode = sg.LISTBOX_SELECT_MODE_MULTIPLE )],
-            [sg.Button('OK')],
+
+
+
+layout1 = [  [sg.Text('Choose recipes:', font = ('default', 11, 'bold'))],
+            [sg.Listbox( values= all_filenames_as_list_of_str, size=(30, 6), select_mode = sg.LISTBOX_SELECT_MODE_MULTIPLE ), sg.Button ('OK')],
             [sg.Text('Your shopping list:', font = ('default', 11, 'bold'))],
             [sg.Multiline('Select some recipes and click OK', font = ('default', 10, 'italic'), size=(45,20))],
-            [sg.Button('Close')] ]
+            [sg.Button('Help'), sg.Button('Close')]]
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+
+
+def help_window ():
+    with open(help_text_full_path, 'r') as file:
+
+        layout2 = [  [sg.Text('Help')],
+                    [sg.Multiline(default_text=file.read(), disabled=True, size=(70,10))],
+                    [sg.Button('Back')]]
+
+        window2 = sg.Window('Help',layout2)
+        while True:
+            event2, values2 = window2.read()
+            if event2 == sg.WIN_CLOSED or event2 == 'Back':
+                break
+        window2.close()
+
+
+
+# Create the main Window
+window1 = sg.Window('Recipes project', layout1)
+
 while True:
-    event, values = window.read()
+    event1, values1 = window1.read()
 
-    if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
+    if event1 == sg.WIN_CLOSED or event1 == 'Close':
         break
 
-    selected_filenames =  values[0]
+    selected_filenames =  values1[0]
 
     from_filenames_to_shopping_list (selected_filenames, recipes_full_path, result_full_path)
 
-    with open (os.path.join(result_full_path, "myshoppinglist.txt")) as file:  #?????
+    if event1 == 'Help':
+        help_window()            
+
+    with open (os.path.join(result_full_path, "myshoppinglist.txt")) as file: 
         file.readline()
         shopping_list = file.read()
 
-    window[1].update(shopping_list, font = ('default', 10, 'normal'))
+    window1[1].update(shopping_list, font = ('default', 10, 'normal'))
     
-window.close()
+window1.close()
--- main/paths_config.py
+++ main/paths_config.py
@@ -1,9 +1,10 @@
 import os
 
 absolute_path_main = os.path.dirname(__file__)
-recipes_full_path = os.path.join(absolute_path_main, "text", "recipes")
-result_full_path = os.path.join(absolute_path_main, "text", "result")
-readme_full_path = os.path.join(absolute_path_main, '..', "README.md")
-shopping_list_full_path = os.path.join(result_full_path, "myshoppinglist.txt")
+recipes_full_path = os.path.join(absolute_path_main, 'text', 'recipes')
+result_full_path = os.path.join(absolute_path_main, 'text', 'result')
+readme_full_path = os.path.join(absolute_path_main, '..', 'README.md')
+help_text_full_path = os.path.join(absolute_path_main, 'text', 'help_text', 'help_text.txt')
+shopping_list_full_path = os.path.join(result_full_path, 'myshoppinglist.txt')
 
 os.makedirs(result_full_path, exist_ok=True)
