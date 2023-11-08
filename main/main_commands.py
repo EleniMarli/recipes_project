6ec2b1e151cddba7a06f44135bf975a43b575235
Eleni Marli <elenimarli@yahoo.gr>
2023-11-08 17:25:23 +0100

    Check if user input is numeric and positive for choose_files method

--- main/main_commands.py
+++ main/main_commands.py
@@ -4,32 +4,44 @@ import subprocess
 from main.recipe_module import Recipe
 from main.shopping_list_module import Shopping_list
 from main.paths_config import recipes_full_path, result_full_path, readme_full_path, permanent_result_folder_full_path
-from main.txt_files_config import remove_txt_from_filenames, add_dot_to_filenames
+from main.txt_files_config import remove_txt_from_filenames, add_front_dot_to_filenames
 
 
 
 def choose_files (all_filenames_as_list_of_str_local):
  
     while True:
-        print("\nThe following are the available recipes:")
 
-        dictionary = {}
-        counter = 1
-        for file_name in all_filenames_as_list_of_str_local:
-            dictionary[counter] = file_name
-            print(f'{counter} :' + f' {file_name.replace(".txt","")}' )
-            counter += 1
+        while True:
+            print("\nThe following are the available recipes:")
 
-        print("\nPlease choose the recipes to be considered by typing the corresponding digit. Separate the digits using one space." )
+            dictionary = {}
+            counter = 1
+            for file_name in all_filenames_as_list_of_str_local:
+                dictionary[counter] = file_name
+                print(f'{counter} :' + f' {file_name.replace(".txt","")}' )
+                counter += 1
 
-        user_input_local = input()
+            print("\nPlease choose the recipes to be considered by typing the corresponding digit. Separate the digits using one space." )
 
+            user_input_local = input()
+
+            user_input_no_spaces = user_input_local.replace(" ", "")
+
+            # control if input is numeric (also False if a minus is included, therefore checks if input is positive as well)
+            if user_input_no_spaces.isdigit() == False:
+                print('Please type one or more valid digits and separate them using one space." )')
+                continue
+            else:
+                break
+          
         list_of_int = list (map (lambda x: int(x), user_input_local.split(' ')))
 
         called_filenames_list_local = []
         for integr in list_of_int:
             called_filenames_list_local += [dictionary.get(integr)]
 
+        # control if input is a valid digit
         if None in called_filenames_list_local:
             print(f"\nOne or more digits you have inserted do not correspond to a recipe. Try again.")
         else:
@@ -37,7 +49,7 @@ def choose_files (all_filenames_as_list_of_str_local):
 
     print("\nThe selected recipes are the following:")
     remove_txt_from_filenames(called_filenames_list_local)
-    print(*add_dot_to_filenames(remove_txt_from_filenames(called_filenames_list_local)), sep='\n')
+    print(*add_front_dot_to_filenames(remove_txt_from_filenames(called_filenames_list_local)), sep='\n')
 
     return called_filenames_list_local
 
@@ -71,19 +83,19 @@ def execute_app():
 
         print("\nAre you sure? (yes/no/help)")
 
-        user_input1 = input()
+        user_input1 = input()   #1
         if user_input1 == 'yes':
             list_for_use = from_filenames_to_temporary_shopping_list (called_filenames_list, recipes_full_path, result_full_path)
 
             while True:
                 print ('\nIf you run the application again, your current shopping list will be overwritten. Do you want to save it? (yes/no)')
 
-                user_input2 = input()
+                user_input2 = input()  #2
 
                 if user_input2 == 'yes':
                     while True:
                         print('\nHow do you want to name this shopping list?')
-                        filename = input()
+                        filename = input()  #3
                         current_permanent_shopping_list_full_path = os.path.join(permanent_result_folder_full_path, filename + '.txt')
                         if os.path.exists(current_permanent_shopping_list_full_path) == True:
                             print(f'File with name {filename} already exists. Try an other name.')
